9beb7de43cf54a9054ada6df230684864464339b
Ankit Pandey <113189919+pandeyankit123@users.noreply.github.com>
2024-06-29 04:39:01 +0530

    Update project.js

--- src/components/Data/project.js
+++ src/components/Data/project.js
@@ -5,6 +5,7 @@ export const ProjectData = [
     about:
       "Manages student records with CRUD operations, pagination, modal UI for seamless editing, Excel import/export, searching/sorting/filtering, bcrypt/JWT for auth and displaying of result and downloading same in customized PDF forma. Developed in TypeScript, ensuring type safety and improved code quality.",
     tags: ["MongoDB", "React.js", "Node.js", "JWT", "bcrypt.js", "html2pdf", "XLSX"],
+    demo: "https://result-portalrp.vercel.app/",
     github: "https://github.com/pandeyankit123/Result-Portal-with-TS",
     image: [require("../../assets/rp1.png"), require("../../assets/rp2.png"), require("../../assets/rp3.png"), require("../../assets/rp4.png")],
   },
@@ -14,7 +15,7 @@ export const ProjectData = [
     about:
       "This chat application allows users to join chat rooms, send and receive messages in real-time, and see who else is currently online. Built with React, Express, Node.js, and Socket.IO, it offers a seamless chat experience with features like room selection, message input, and user list display.",
     tags: ["React.js", "Node.js", "Socket.io", "CORS"],
-    // demo: "https://pandeyankit123.github.io/React-TextUtils/",
+    demo: "https://chat-application-pandeyankit123.vercel.app/",
     github: "https://github.com/pandeyankit123/Chat-Application",
     image: [require("../../assets/ca1.png"),require("../../assets/ca2.png"), require("../../assets/ca3.png")],
   }
